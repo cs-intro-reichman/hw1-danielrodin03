873ce028e4b788a9b6dfcf3f6a96517e5cc014c7
Daniel Rodin <danielrodin@MacBook-Pro-sl-Daniel.local>
2025-10-30 19:18:42 +0200

    done

--- FVCalc.java
+++ FVCalc.java
@@ -5,6 +5,6 @@ public class FVCalc {
         double rate = Double.parseDouble(args [1]);
         int year = Integer.parseInt(args [2]);
         double futureValue = currentValue * Math.pow((1 + (rate / 100)), year);
-        System.out.println("After " +year+ " years, " +currentValue+ "$ saved at " +rate+ "% will yield " +(int)futureValue+ "$");
+        System.out.println("After " +year+ " years, $" +currentValue+ " saved at " +rate+ "% will yield $" +(int)futureValue);
 	}
 }
